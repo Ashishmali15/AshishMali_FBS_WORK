8234ae9ce311573de52a3378d1fac7addbff97c4
Ashishmali15 <maliashish154@gmail.com>
2025-10-11 09:48:56 +0530

    Add files via upload

---
+++ C-programming/Classwork/type-1 Function/areabyfunc.c
@@ -0,0 +1,12 @@
+void area1();
+void main(){
+	 area1();
+}
+
+void area1(){
+		float r,PI=3.142,area;
+		printf("Enter the radius= ");
+		scanf("%f",&r);
+		area=PI*r*r;
+		printf("area of circle=%f",area);	
+}
